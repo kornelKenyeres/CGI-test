0e443c6c348043809943dd1f6b04cf8acf341bee
Kornel Kenyeres <sk84cer@gmail.com>
2022-06-14 18:50:17 +0200

    implementations added

--- src/main/java/com/cgi/boat/interview/Main.java
+++ src/main/java/com/cgi/boat/interview/Main.java
@@ -1,24 +1,73 @@
 package com.cgi.boat.interview;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
+	
+	public static final Integer TOP_COUNT = 3;
 
     public static void main(String[] args) {
         Map<String, List<String>> firstByLast = PeopleProcessor.firstnamesByLastname(PeopleSetup.people);
         Map<String, List<String>> lastByFirst = PeopleProcessor.lastnamesByFirstname(PeopleSetup.people);
-
+        
         // TODO: Print out 3 most common first names along with number of occurrences
         // for example:
         // Homer: 32
         // Bart: 21
         // William: 3
+
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+        
+        for (String key : lastByFirst.keySet()) {
+        	int currentSize = lastByFirst.get(key).size();
+        	if (result.size() < TOP_COUNT) {
+        		
+        		//initial set with first ${TOP_COUNT} items
+        		result.put(key, currentSize);
+        	
+        	} else if (checkIfMorePopular(result, currentSize)) {
+        	
+        		//if a name is more popular, then one of the ${TOP_COUNT} most popular names so far
+        		//then swap current with the less popular one
+        		swapWithLowest(result, key, currentSize);
+        	
+        	}
+        }
+        
+        //print result
+        for (String key : result.keySet()) {
+        	System.out.println(key+": "+result.get(key));
+        }
     }
 
+    //Checks if the name is more popular then the current tops
+    private static boolean checkIfMorePopular(Map<String, Integer> top, int size) {
+    	for (String key : top.keySet()) {
+    		if (size > top.get(key)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
 
-
-
+    //swaps the less popular element in the "result" array with the new one (only if it is more popular)
+    private static HashMap<String, Integer> swapWithLowest(HashMap<String, Integer> top, String name, int occourance) {
+    	String lowestName = null;
+    	int lowestCount = Integer.MAX_VALUE;
+    	for (String key : top.keySet()) {
+    		if (top.get(key) < lowestCount) {
+    			lowestCount = top.get(key);
+    			lowestName = key;
+    		}
+    	}
+    	if (lowestCount < occourance) {
+    		top.remove(lowestName);
+    		top.put(name, occourance);
+    	}
+    	return top;
+    }
 
 
 
--- src/main/java/com/cgi/boat/interview/PeopleProcessor.java
+++ src/main/java/com/cgi/boat/interview/PeopleProcessor.java
@@ -1,5 +1,8 @@
 package com.cgi.boat.interview;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,7 +21,26 @@ class PeopleProcessor {
      * }
      */
     static Map<String, List<String>> lastnamesByFirstname(List<Person> people){
-        //TODO: implement
+    	
+    	List<Person> sorted = new ArrayList<Person>(people);
+    	sorted.sort(Comparator.comparing(Person::getFirstName));
+    	Map<String, List<String>> result = new HashMap<String, List<String>>();
+    	String currentFirstName = people.get(0).getFirstName();
+    	List<String> lastNames = new ArrayList<String>();
+    	
+    	for (Person p : sorted) {
+    		if (p.getFirstName().equals(currentFirstName)) {
+    			lastNames.add(p.getLastName());
+    		} else {
+    			result.put(currentFirstName, lastNames);
+    			lastNames = new ArrayList<String>();
+    			currentFirstName = p.getFirstName();
+    			lastNames.add(p.getLastName());
+    		}
+    	}
+    	result.put(currentFirstName, lastNames);
+    	
+    	return result;
     }
 
 
@@ -35,7 +57,26 @@ class PeopleProcessor {
      *
      */
     static Map<String, List<String>> firstnamesByLastname(List<Person> people){
-        //TODO: implement
+    	
+    	List<Person> sorted = new ArrayList<Person>(people);
+    	sorted.sort(Comparator.comparing(Person::getLastName));
+    	Map<String, List<String>> result = new HashMap<String, List<String>>();
+    	String currentLastName = people.get(0).getLastName();
+    	List<String> firstNames = new ArrayList<String>();
+    	
+    	for (Person p : sorted) {
+    		if (p.getLastName().equals(currentLastName)) {
+    			firstNames.add(p.getFirstName());
+    		} else {
+    			result.put(currentLastName, firstNames);
+    			firstNames = new ArrayList<String>();
+    			currentLastName = p.getLastName();
+    			firstNames.add(p.getFirstName());
+    		}
+    	}
+    	result.put(currentLastName, firstNames);
+    	
+    	return result;
     }
 
 }
